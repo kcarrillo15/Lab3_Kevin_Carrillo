fe845b581ea1636023f590fb759fb0067a60b5ba
Kevin <kcarrillo15@student.ccc.edu>
2024-09-21 22:22:55 -0500

    AHHHHHH TGHE PROBLEM WAS SO EASY TO FIX

--- File1.py
+++ File1.py
@@ -1,8 +1,9 @@
 i = 1
-while i in range(11):
-    if i == 8: break
+while i <= 10:
+    if i == 7:
+        break
     print(i)
-    i +=1 
+    i += 1
     
 word = "CIS103Lab3"
 for i in range(len(word)):
@@ -12,3 +13,13 @@ for i in range(1, 6):
     for j in range(i):
         print("*", end="")
     print()
+    
+i = 1
+while i <= 10:
+    if i == 5:
+        i += 1
+        continue
+    if i == 7:
+        break
+    print(i)
+    i += 1
